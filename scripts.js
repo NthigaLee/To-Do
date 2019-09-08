71d65cde16c22745d3dc9be8f1aa7742dab7e45d
NthigaLee <nthigalee@gmail.com>
2019-09-08 10:08:56 +0300

    First commit with JS and HTML elements

---
+++ scripts.js
@@ -0,0 +1,31 @@
+function List(plan,time,date,notes){
+    this.doPlan = plan;
+    this.doTime= time;
+    this.doDate  = date;
+    this.doNotes = notes;
+}
+
+$(document).ready(function(){
+    $("form#new-todo").submit(function(event){
+        event.preventDefault();
+
+        var userPlan = $("input#new-plan").val();
+        var userTime = $("input#new-time").val().toString();
+        var userDate = $("input#new-date").val().toString();
+        var userNotes = $("textarea#new-notes").val();
+
+        var newList = new List(userPlan,userTime,userDate,userNotes)
+
+        $("ul#to-do").append("<li><span class = 'newones'>"+ newList.doPlan+", on "+ newList.doDate+"</span></li>" );
+        $(".newones").last().click(function(){
+            $("#show-list").fadeToggle();
+            $("#show-list h2").text(newList.doPlan);
+            $(".plan").text(newList.doPlan);
+            $(".date").text(newList.doDate);
+            $(".time").text(newList.doTime);
+            $(".notes").text(newList.doNotes);
+            
+        });
+
+    });
+});
